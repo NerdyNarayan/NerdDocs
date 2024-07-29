2e85a52bd22917885ee93a406c6d8924750a5767
NerdyNarayan <nerdynarayan@gmail.com>
2024-07-29 17:56:50 +0530

    feat: intial setup

---
+++ components/home/hero.tsx
@@ -0,0 +1,29 @@
+'use client';
+import type { FC } from 'react';
+
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+import { SiteConfig } from 'config/site';
+const Hero: FC = () => {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <h1 className="text-accent-foreground text-8xl font-bold">
+          {SiteConfig.name}
+        </h1>
+        <p className="text-2xl text-primary/80">{SiteConfig.description}</p>
+        <a
+          className="text-primary underline"
+          href={SiteConfig.links.github}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaGithub className="text-3xl" />
+        </a>
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export const SiteConfig = {
+  name: 'NerdyDocs',
+  description: 'Docs and guides for nerds.',
+  links: {
+    github: 'https://github.com/NerdyNaryana/NerdyDocs'
+  }
+};
